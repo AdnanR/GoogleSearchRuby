ca4c8939d86a1b70faabdfaf318673c26746616b
Adnan Riaz <AdnanR@users.noreply.github.com>
2016-07-20 09:54:11 +1000

    remove commented code

--- features/step_definitions/step_definitionts.rb
+++ features/step_definitions/step_definitionts.rb
@@ -1,6 +1,4 @@
 Given(/^I am on the Google home page$/) do
-  #TestRun.browser.goto TestRun.baseurl
-  #@browser.goto "http://www.google.com"
   @search_page = GoogleSearch.new
 end
 
@@ -15,7 +13,7 @@ Then(/^the (\d+)(?:st|nd|rd|th) search result should contain the ([^"]*) text$/)
    # Verify search results were displayed
    expect(results).not_to be_nil, "No search results were displayed."
 
-   # // Verify number of results is at least what we expect
+   # Verify number of results is at least what we expect
    expect(results.length).to be >= result_number.to_i, "Results count is less then expected. Actual #{results.length}, Expected #{result_number} (at least)."
 
    # Verify that required result element contains search term.
